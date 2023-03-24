959bd8971b78b17d71170ed66b7b26d75e16112a
PcePvP <shoaibazad20@gmail.com>
2023-03-24 02:48:17 +0000

    Update PdfGenerate.java

--- src/application/PdfGenerate.java
+++ src/application/PdfGenerate.java
@@ -26,7 +26,43 @@ public class PdfGenerate {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        
+        // Create a new PDF document
+        Document document = new Document(PageSize.A4);
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("report.pdf"));
+            document.open();
+            
+            // Add a title to the document
+            Paragraph title = new Paragraph("Inventory Report");
+            document.add(title);
+            
+            // Retrieve data from the orders table
+            PreparedStatement ordersStmt = conn.prepareStatement("SELECT * FROM user_orders WHERE order_status='placed'");
+            ResultSet ordersResult = ordersStmt.executeQuery();
+            
+            // Create a table for the placed orders
+            PdfPTable placedOrdersTable = new PdfPTable(3);
+            PdfPCell cell;
+            cell = new PdfPCell(new Paragraph("User ID"));
+            placedOrdersTable.addCell(cell);
+            cell = new PdfPCell(new Paragraph("Customer Name"));
+            placedOrdersTable.addCell(cell);
+            cell = new PdfPCell(new Paragraph("Order Total"));
+            placedOrdersTable.addCell(cell);
+            
+            while (ordersResult.next()) {
+            	placedOrdersTable.addCell(Integer.toString(ordersResult.getInt("user_id")));
+            	placedOrdersTable.addCell(ordersResult.getString("name"));
+                placedOrdersTable.addCell(Double.toString(ordersResult.getDouble("price")));
+            }
+            
+            // Add the placed orders table to the document
+            document.add(new Paragraph("The following items need to be dispatched:"));
+            document.add(placedOrdersTable);
 
+            // Close the document and the database connection
+            document.close();
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
